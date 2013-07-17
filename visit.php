1df9d8b2f7bc36f592aa7c76b220421306bf31f1
fundead <callum@mindscape.co.nz>
2013-07-17 12:46:36 +1200

    Further incorrect PHP version check fix

---
+++ visit.php
@@ -0,0 +1,11 @@
+<style type='text/css'>
+#rgFrame
+{
+    display: block;
+    margin: auto;
+    width: 95%;
+    height: 100%;
+}
+</style>
+
+<div><iframe src='http://raygun.io/features' id='rgFrame'></iframe></div>
