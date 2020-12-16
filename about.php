90081ffbba3e91ab0ca2e25571a4827d43c1e9ca
izsi-salmon <izsi.salmon@gmail.com>
2020-12-16 14:02:43 +1300

    update app url

--- about.php
+++ about.php
@@ -36,7 +36,7 @@
 
     <ol>
       <li><a href="https://app.raygun.com/signup?utm_source=link&amp;utm_medium=text&amp;utm_campaign=wp-plugin-setup" target="_blank">Create a Raygun account</a></li>
-      <li>Go to the <a href="http://app.raygun.com/dashboard" target="_blank">Raygun dashboard</a> and create a new application. <span class="rg4wp-helptip">This represents your website</span></li>
+      <li>Go to the <a href="http://app.raygun.com" target="_blank">Raygun dashboard</a> and create a new application. <span class="rg4wp-helptip">This represents your website</span></li>
       <li>Activate Crash Reporting and RUM</li>
       <li>Copy the API Key <span class="rg4wp-helptip">Include the '==' at the end</span></li>
       <li>Go to the <a href="<?php menu_page_url('rg4wp-settings', true); ?>">Raygun4WP Settings page</a></li>
