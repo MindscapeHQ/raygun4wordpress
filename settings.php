1afee070ce60e96274fdc5be015805bbfe5b084b
Jared Scholz <jaredsc108@gmail.com>
2023-01-24 15:51:36 +1300

    Added rg4wp_admintracking to page options

--- settings.php
+++ settings.php
@@ -154,7 +154,7 @@
       </table>
 
       <input type="hidden" name="action" value="update" />
-      <input type="hidden" name="page_options" value="rg4wp_status,rg4wp_apikey,rg4wp_tags,rg4wp_404s,rg4wp_js,rg4wp_usertracking,rg4wp_ignoredomains,rg4wp_pulse,rg4wp_js_tags" />
+      <input type="hidden" name="page_options" value="rg4wp_status,rg4wp_apikey,rg4wp_tags,rg4wp_404s,rg4wp_js,rg4wp_usertracking,rg4wp_ignoredomains,rg4wp_pulse,rg4wp_js_tags,rg4wp_admintracking" />
 
       <p class="submit">
         <?php
